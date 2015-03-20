06df3a4e33cd5d9c39191ea3ca5ded9d19de7e9b
Alwin Mao <amao@tzab.berkeley.edu>
2015-03-19 23:50:01 -0700

    Pulling

--- calc_maxjutt.f90
+++ calc_maxjutt.f90
@@ -1,34 +1,43 @@
        module calc_maxjutt
-!       use math
-!        use polsynchemis, only: initialize_polsynchpl,polsynchpl, &
-!        polsynchth,del_polsynchpl,synchpl,bnu,synchemis,synchemisnoabs
+
         use polsynchemis, only: polsynchth
 
        contains
-!         subroutine calc_maxjutt_subroutine(nsim,usim,maxjutt_args,maxjutt_thetas,mj_ncgs,mj_tcgs)
-         subroutine calc_maxjutt_subroutine(nsim,usim,nu,bcgs,incang,maxjutt_args,ktotal)
-           double precision, dimension(:), intent(in) :: nsim,usim
-           double precision, dimension(:), intent(in) :: nu,bcgs,incang
+
+         subroutine calc_maxjutt_subroutine(nu,ncgs,bcgs,tcgs,usim,incang,maxjutt_args,ktotal)
+         subroutine calc_maxjutt_subroutine(nu,ncgs,bcgs,tcgs,incang,maxjutt_args,ktotal)
+           double precision, dimension(:), intent(in) :: nu
+           double precision, dimension(:), intent(in) :: ncgs,bcgs,tcgs,incang
            double precision, dimension(:), intent(in) :: maxjutt_args
 !6 weights, 1 delta
+           double precision, dimension(:), intent(in) :: usim
 
-!           double precision, intent(in) :: nu,bcgs,incang
-!           double precision, dimension(size(nsim)) :: ones
+
            double precision, dimension(size(maxjutt_args) - 1) :: weights_arr, i_arr, delta_arr
-
-           double precision, dimension(size(nsim)) :: theta_min
+           double precision, dimension(size(ncgs)) :: tcgs_min
+!           double precision, dimension(size(ncgs)) :: theta_min
            double precision :: delta, a, dwsum
 
-           double precision, dimension(size(nsim),11) :: ktemp
-           double precision, dimension(size(nsim),11),intent(out) :: ktotal
+           double precision, dimension(size(ncgs),11) :: ktemp
+           double precision, dimension(size(ncgs),11),intent(out) :: ktotal
 !           ones = 1d0
            a = 3d0
+!Note derivation assumes relativistic a = 3.0
            delta = maxjutt_args(1) !first argument is delta
            weights_arr = maxjutt_args(2:)/SUM(maxjutt_args(2:)) !normalize
            i_arr = (/ (I, I=0,size(weights_arr)-1) /) !indices
            delta_arr = delta**i_arr !all deltas to be multiplied by thetamin
            dwsum = SUM(weights_arr * delta_arr)
-           theta_min = (usim/nsim)/a/dwsum 
+
+!           theta_min = (usim/ncgs)/a/dwsum !USIM
+
+!u_e = 1/3 ncgs k tcgs, theta_min = 1/3 k tcgs / m / c / c
+!          theta_min = k * tcgs/a/dwsum/a/m/c/c !I use a as 3 
+!           tcgs_min = m * c * c / k * theta_min
+           tcgs_min = tcgs/a/a/dwsum
+!check if that second a is necessary
+
+
 !           write(6,*) weights_arr
 !           write(6,*) delta_arr
 !N array of theta_mins calculated to satisfy sum energy = usim
@@ -46,7 +55,7 @@
 
            do i=1,size(weights_arr)
               ktemp = 0d0
-              call polsynchth(nu,weights_arr(i)*nsim,bcgs,theta_min*delta_arr(i),incang,ktemp)
+              call polsynchth(nu,weights_arr(i)*ncgs,bcgs,tcgs_min*delta_arr(i),incang,ktemp)
               ktotal = ktotal + ktemp
 !ktotal = ktotal + ktemp
            enddo
