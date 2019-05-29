1e0a9462ce0c3d62c8ad45f5c6b2a853c9dfd04b
Jason Dexter <jdexter@mpe.mpg.de>
2019-05-29 11:32:05 +0200

    try again

--- fluid_model_harm3d.f90
+++ fluid_model_harm3d.f90
@@ -211,7 +211,7 @@
 !        endwhere
 !        where(zr.le.(1.+sqrt(1.-a**2.)))
 ! changing this to be inside of uniqr(1)
-        where(zr.le.maxval(uniqr(1),(1.+sqrt(1.-a**2.))))
+        where(zr.le.max(uniqr(1),(1.+sqrt(1.-a**2.))))
            rd=1.
            pfac=1d-6
            nfac=1d-6
